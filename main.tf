1a3b79e62bfbf3a49e5f3bcb619ef193cc1a9ce8
Rodrigo Figueira <rodrigoservice@gmail.com>
2025-01-13 16:19:31 -0300

    add roles para lambda

--- main.tf
+++ main.tf
@@ -26,3 +26,30 @@ resource "aws_dynamodb_table" "dynamo_table" {
 
 
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_dynamodb_cloudwatch_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action   = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
